f271e682b5848ff320fe57bbb3553772ee4c72b9
Tomek Michalski <tmm.michalski@gmail.com>
2022-03-29 19:17:15 +0200

    add Element.py

--- 06_projekt_gracz/main.py
+++ 06_projekt_gracz/main.py
@@ -1,34 +1,5 @@
 import pygame
-
-
-class Obraz(pygame.sprite.Sprite):
-    def __init__(self, sciezka):
-        super(Obraz, self).__init__()
-        self.obraz = pygame.image.load(sciezka)
-
-
-class Element():
-    def __init__(self, typ):
-        self.wybrany = 0
-        self.lista_obrazow = []
-
-        for i in range(1, 4):
-            sciezka = f"images/{typ}{i}.png"
-            self.lista_obrazow.append(Obraz(sciezka))
-
-    def wybierz_nastepny(self):
-        self.wybrany += 1
-        if self.wybrany > 2:
-            self.wybrany = 0
-
-    def podaj_wybrany_obraz(self):
-        return self.lista_obrazow[self.wybrany].obraz
-
-
-class NakrycieGlowy(Element):
-    def __init__(self):
-        super().__init__('head')
-
+import Element
 
 SZEROKOSC_EKRANU = 800
 WYSOKOSC_EKRANU = 600
@@ -36,6 +7,12 @@ WYSOKOSC_EKRANU = 600
 # importowanie zasobów z folderu
 obraz_tla = pygame.image.load("images/background.png")
 obraz_bazy_postaci = pygame.image.load("images/base.png")
+# elementy stroju
+nakrycie_glowy = Element.NakrycieGlowy()
+ubranie_element = Element.UbranieElement()
+oczy_element = Element.OczyElement()
+bron_element = Element.BronElement()
+
 
 pygame.init()
 
@@ -48,13 +25,30 @@ while gra_dziala:
 
     for zdarzenie in pygame.event.get():
         if zdarzenie.type == pygame.KEYDOWN:
+            
             if zdarzenie.key == pygame.K_ESCAPE:
                 gra_dziala = False
+            elif zdarzenie.key == pygame.K_q:
+                nakrycie_glowy.wybierz_nastepny()
+            elif zdarzenie.key == pygame.K_w:
+                oczy_element.wybierz_nastepny()
+            elif zdarzenie.key == pygame.K_e:
+                ubranie_element.wybierz_nastepny()
+            elif zdarzenie.key == pygame.K_r:
+                bron_element.wybierz_nastepny()
+
+
         elif zdarzenie.type == pygame.QUIT:
             gra_dziala = False
 
     ekran.blit(obraz_tla, (0, 0))
     ekran.blit(obraz_bazy_postaci, (270, 130))
+
+    # wyrenderuj ubranka i akcesoria
+    ekran.blit(nakrycie_glowy.podaj_wybrany_obraz(), (270, 130))
+    ekran.blit(ubranie_element.podaj_wybrany_obraz(), (270, 130))
+    ekran.blit(oczy_element.podaj_wybrany_obraz(), (270, 130))
+    ekran.blit(bron_element.podaj_wybrany_obraz(), (270, 130))
 
     pygame.display.flip()
 
