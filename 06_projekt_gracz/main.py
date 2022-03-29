4ed08b923a43059965edf9063f9419d74fe81d0a
Tomek Michalski <tmm.michalski@gmail.com>
2022-03-29 19:58:43 +0200

    finish project

--- 06_projekt_gracz/main.py
+++ 06_projekt_gracz/main.py
@@ -20,12 +20,21 @@ ekran = pygame.display.set_mode([SZEROKOSC_EKRANU, WYSOKOSC_EKRANU])
 zegar = pygame.time.Clock()
 
 gra_dziala = True
+zapisywanie = False
+
+
+def wypisz_tekst(ekran, tekst, pozycja):
+    pygame.font.init()
+    moja_czcionka = pygame.font.SysFont('Comic Sans MS', 30)
+    napis = moja_czcionka.render(tekst, False, (255, 255, 255))
+    ekran.blit(napis, pozycja)
+
 
 while gra_dziala:
 
     for zdarzenie in pygame.event.get():
         if zdarzenie.type == pygame.KEYDOWN:
-            
+
             if zdarzenie.key == pygame.K_ESCAPE:
                 gra_dziala = False
             elif zdarzenie.key == pygame.K_q:
@@ -36,7 +45,8 @@ while gra_dziala:
                 ubranie_element.wybierz_nastepny()
             elif zdarzenie.key == pygame.K_r:
                 bron_element.wybierz_nastepny()
-
+            elif zdarzenie.key == pygame.K_s:
+                zapisywanie = True
 
         elif zdarzenie.type == pygame.QUIT:
             gra_dziala = False
@@ -49,6 +59,17 @@ while gra_dziala:
     ekran.blit(ubranie_element.podaj_wybrany_obraz(), (270, 130))
     ekran.blit(oczy_element.podaj_wybrany_obraz(), (270, 130))
     ekran.blit(bron_element.podaj_wybrany_obraz(), (270, 130))
+
+    if zapisywanie:
+        pygame.image.save(ekran, 'postac.png')
+        zapisywanie = False
+
+    # renderowanie instrukcji
+    wypisz_tekst(ekran, f"[Q] Glowa: {nakrycie_glowy.wybrany}", (100, 100))
+    wypisz_tekst(ekran, f"[W] Oczy: {oczy_element.wybrany}", (100, 140))
+    wypisz_tekst(ekran, f"[E] Stroj: {ubranie_element.wybrany}", (100, 180))
+    wypisz_tekst(ekran, f"[R] Bron: {bron_element.wybrany}", (100, 220))
+    wypisz_tekst(ekran, "[S] Zapisz", (100, 260))
 
     pygame.display.flip()
 
