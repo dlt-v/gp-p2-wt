19e91af4bc537c380168e4827c14b3ddf66dfa8f
Delta.v <tmm.michalski@gmail.com>
2022-05-17 19:25:43 +0200

    update

--- 11_12_13_arkanoid/main.py
+++ 11_12_13_arkanoid/main.py
@@ -9,7 +9,10 @@ ekran = pygame.display.set_mode([SZEROKOSC_EKRANU, WYSOKOSC_EKRANU])
 zegar = pygame.time.Clock()
 obraz_tla = pygame.image.load("images/background.png")
 
+# Tworzymy zmienną platforma, która przechowuje instancje klasy Platforma.
 platforma = Platforma()
+
+kulka = Kulka()
 
 gra_dziala = True
 
@@ -21,11 +24,6 @@ while gra_dziala:
             if zdarzenie.key == pygame.K_ESCAPE:
                 gra_dziala = False
 
-            # elif zdarzenie.key == pygame.K_a:
-            #     platforma.ruszaj_platforma(-1)
-            # elif zdarzenie.key == pygame.K_d:
-            #     platforma.ruszaj_platforma(1)
-
         # naciśnięcie krzyżyka na oknie aplikacji
         elif zdarzenie.type == pygame.QUIT:
             gra_dziala = False
@@ -36,6 +34,10 @@ while gra_dziala:
         platforma.ruszaj_platforma(-1)
     elif keys[pygame.K_d]:
         platforma.ruszaj_platforma(1)
+
+    # aktualizacja pozycji
+    kulka.aktualizuj(platforma)
+    platforma.aktualizuj()
 
     # renderowanie obiektów na ekranie
     ekran.blit(obraz_tla, (0, 0))
